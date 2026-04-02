2d789520cd76dc6d9c5f4aa2bc044fa222bd8650
antim0118 <my.antim@inbox.ru>
2026-04-02 08:08:32 +0500

    Update FileStream.h

--- src/utils/FileStream.h
+++ src/utils/FileStream.h
@@ -4,27 +4,24 @@
 #include <algorithm>
 #include <filesystem>
 
-class FileStream : std::fstream {
+class FileStream : std::fstream
+{
 public:
     FileStream()
-        : std::fstream()
-    {
+        : std::fstream() {
     }
 
-    FileStream(const std::filesystem::path& file, std::ios::openmode mode)
-        : std::fstream(file, mode)
-    {
+    FileStream(const std::filesystem::path &file, std::ios::openmode mode)
+        : std::fstream(file, mode) {
     }
 
-    ~FileStream()
-    {
+    ~FileStream() {
         std::fstream::close();
     }
 
     template <class T>
-    void readVariable(T* p, bool swapEndianness = false, std::size_t size = 0)
-    {
-        char* pChar = reinterpret_cast<char*>(p);
+    void readVariable(T *p, bool swapEndianness = false, std::size_t size = 0) {
+        char *pChar = reinterpret_cast<char *>(p);
         if (!size) {
             size = sizeof(T);
         }
@@ -35,33 +32,28 @@ public:
     }
 
     template <class T>
-    void writeVariable(T* p, std::size_t size = 0)
-    {
-        char* pChar = reinterpret_cast<char*>(p);
+    void writeVariable(T *p, std::size_t size = 0) {
+        char *pChar = reinterpret_cast<char *>(p);
         if (!size) {
             size = sizeof(T);
         }
         write_impl(pChar, size);
     }
 
-    virtual bool isOpen()
-    {
+    virtual bool isOpen() {
         return std::fstream::is_open();
     }
 
-    virtual void setPos(std::streampos pos)
-    {
+    virtual void setPos(std::streampos pos) {
         std::fstream::seekg(pos);
     }
 
 private:
-    virtual void read_impl(char* s, std::streamsize n)
-    {
+    virtual void read_impl(char *s, std::streamsize n) {
         std::fstream::read(s, n);
     }
 
-    virtual void write_impl(char* s, std::streamsize n)
-    {
+    virtual void write_impl(char *s, std::streamsize n) {
         std::fstream::write(s, n);
     }
 };
